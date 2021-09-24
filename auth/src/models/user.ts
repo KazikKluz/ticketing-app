8ed3fad41ae6838d002206a69ca4d1b1a6c0200b
KazikKluz <kazik.kluz@gmail.com>
2021-09-24 12:58:27 +0100

    type checking user properties

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+//an interface that describes the properties
+//that are required to create a new User
+interface UserAttr {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -13,4 +20,8 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export { User };
+const buildUser = (attrs: UserAttr) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
