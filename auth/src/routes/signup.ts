5b7e6c0b3c3c98f7ab8450bb1e29550c85ab10aa
KazikKluz <kazik.kluz@gmail.com>
2021-09-26 15:50:44 +0100

    user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -23,11 +24,18 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating a user ...');
+    const existingUser = await User.findOne({ email });
 
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
