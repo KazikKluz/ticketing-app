43c014bfd840edbceab602d06de73dd2edd2d37d
KazikKluz <kazik.kluz@gmail.com>
2021-09-30 19:56:49 +0100

    mongoose pre-save hooks

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -12,10 +12,10 @@ export class Password {
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
-    const [hashePassword, salt] = storedPassword.split('.');
+    const [hashedPassword, salt] = storedPassword.split('.');
 
     const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
-    return buf.toString('hex') === hashePassword;
+    return buf.toString('hex') === hashedPassword;
   }
 }
